ef4b0c8db540aaac202404585363440e3faa5fa2
koyumi-takaishi <takaishi.takoyaki@gmail.com>
2021-10-23 00:36:06 +0900

    10/23 first commit

--- WordPressTheme/assets/js/script.js
+++ WordPressTheme/assets/js/script.js
@@ -25,72 +25,90 @@ jQuery(function ($) {
 	}
 	new Swiper('.swiper', swipeOption);
 
-	jQuery(function() {
+	// メインビュー過ぎたらヘッダーの色変わる
+  $(function () {
+    //変数fvHeightを定義
+    var fvHeight = $(".p-mv").innerHeight();
+    var headerHeight = $(".p-header").innerHeight();
+    var changePoint = fvHeight - headerHeight;
+    /*** ヘッダーの色変更 ***/
+  
+    $(window).scroll(function () {
+      //スクロール量とfvの高さを比較
+      if ($(window).scrollTop() > changePoint) {
+        //条件を満たした場合：headerのカラー変更
+        $(".js-header-color-change").addClass("color-change");
+      } else {
+        //条件を満たさない場合：headerのカラーを戻す
+        $(".js-header-color-change").removeClass("color-change");
+      }
+    });
+  });
 
-		// ドロワーメニュー
-		$('.js-hamburger').on('click', function () {
-			if ($('.js-hamburger').hasClass('open')) {
-				$('.js-drawer').fadeOut();
-				$(this).removeClass('open');
-				//  iosスクロール許可(スクロール停止を停止)
-				$('body').css('overflow','');
-				window.removeEventListener( 'touchmove' , move, { passive: false } );
-			} else {
-				$('.js-drawer').fadeIn();
-				$(this).addClass('open');
-				// スクロール停止の処理
-				$('body').css('overflow','hidden');
-				window.addEventListener( 'touchmove' , move , { passive: false } );
-			}
-		});
-
-		/* スムーススクロール */
-		jQuery('a[href^="#"]').click(function() {
-			let header = jQuery(".js-header").height();
-			let speed = 300;
-			let id = jQuery(this).attr("href");
-			let target = jQuery("#" == id ? "html" : id);
-			let position = jQuery(target).offset().top - header;
-			if ("fixed" !== jQuery("#header").css("position")) {
-				position = jQuery(target).offset().top;
-			}
-			if (0 > position) {
-				position = 0;
-			}
-			jQuery("html, body").animate(
-				{
-					scrollTop: position
-				},
-				speed
-			);
-			return false;
-		});
-
-		// スクロール判定
-		// jQuery(window).on("scroll", function() {
-		// 	if (100 < jQuery(this).scrollTop()) {
-		// 		jQuery("body").attr("data-scroll", "true");
-		// 	} else {
-		// 		jQuery("body").attr("data-scroll", "false");
-		// 	}
-		// });
-
-		/* ドロワー */
-		// jQuery(".js-drawer").on("click", function(e) {
-		// 	e.preventDefault();
-		// 	let targetClass = jQuery(this).attr("data-target");
-		// 	jQuery("." + targetClass).toggleClass("is-checked");
-		// 	return false;
-		// });
-
-		/* 電話リンク */
-		// let ua = navigator.userAgent;
-		// if (ua.indexOf("iPhone") < 0 && ua.indexOf("Android") < 0) {
-		// 	jQuery('a[href^="tel:"]')
-		// 		.css("cursor", "default")
-		// 		.on("click", function(e) {
-		// 			e.preventDefault();
-		// 		});
-		// }
+	// ドロワーメニュー
+	$('.js-hamburger').on('click', function () {
+		if ($('.js-hamburger').hasClass('open')) {
+			$('.js-drawer').fadeOut();
+			$(this).removeClass('open');
+			//  iosスクロール許可(スクロール停止を停止)
+			$('body').css('overflow','');
+			window.removeEventListener( 'touchmove' , move, { passive: false } );
+		} else {
+			$('.js-drawer').fadeIn();
+			$(this).addClass('open');
+			// スクロール停止の処理
+			$('body').css('overflow','hidden');
+			window.addEventListener( 'touchmove' , move , { passive: false } );
+		}
 	});
+
+	/* スムーススクロール */
+	jQuery('a[href^="#"]').click(function() {
+		let header = jQuery(".js-header").height();
+		let speed = 300;
+		let id = jQuery(this).attr("href");
+		let target = jQuery("#" == id ? "html" : id);
+		let position = jQuery(target).offset().top - header;
+		if ("fixed" !== jQuery("#header").css("position")) {
+			position = jQuery(target).offset().top;
+		}
+		if (0 > position) {
+			position = 0;
+		}
+		jQuery("html, body").animate(
+			{
+				scrollTop: position
+			},
+			speed
+		);
+		return false;
+	});
+
+	// スクロール判定
+	// jQuery(window).on("scroll", function() {
+	// 	if (100 < jQuery(this).scrollTop()) {
+	// 		jQuery("body").attr("data-scroll", "true");
+	// 	} else {
+	// 		jQuery("body").attr("data-scroll", "false");
+	// 	}
+	// });
+
+	/* ドロワー */
+	// jQuery(".js-drawer").on("click", function(e) {
+	// 	e.preventDefault();
+	// 	let targetClass = jQuery(this).attr("data-target");
+	// 	jQuery("." + targetClass).toggleClass("is-checked");
+	// 	return false;
+	// });
+
+	/* 電話リンク */
+	// let ua = navigator.userAgent;
+	// if (ua.indexOf("iPhone") < 0 && ua.indexOf("Android") < 0) {
+	// 	jQuery('a[href^="tel:"]')
+	// 		.css("cursor", "default")
+	// 		.on("click", function(e) {
+	// 			e.preventDefault();
+	// 		});
+	// }
+
 });
--- WordPressTheme/front-page.php
+++ WordPressTheme/front-page.php
@@ -1,71 +1,74 @@
 <?php get_header(); ?>
 
 <?php
-$home = esc_url( home_url( '/' ) );
-$content = esc_url( home_url( '/content/' ) );
-$philosophy1 = esc_url( home_url( '/content#philosophy1' ) );
-$philosophy2 = esc_url( home_url( '/content#philosophy2' ) );
-$philosophy3 = esc_url( home_url( '/content#philosophy3' ) );
-$news = esc_url( home_url( '/news/' ) );
-$works = esc_url( home_url( '/works/' ) );
-$overview = esc_url( home_url( '/overview/' ) );
-$blog = esc_url( home_url( '/blog/' ) );
-$contact = esc_url( home_url( '/contact/' ) );
+$home = esc_url(home_url('/'));
+$content = esc_url(home_url('/content/'));
+$philosophy1 = esc_url(home_url('/content#philosophy1'));
+$philosophy2 = esc_url(home_url('/content#philosophy2'));
+$philosophy3 = esc_url(home_url('/content#philosophy3'));
+$news = esc_url(home_url('/news/'));
+$works = esc_url(home_url('/works/'));
+$overview = esc_url(home_url('/overview/'));
+$blog = esc_url(home_url('/blog/'));
+$contact = esc_url(home_url('/contact/'));
 ?>
 
 <div class="p-mv l-mv js-mv">
+  <div class="p-mv__wrapper">
+    <div class="p-mv__title">
+      <div class="p-mv__main-title">メインタイトルが入ります</div>
+      <div class="p-mv__sub-title">サブタイトルが入ります</div>
+    </div>
+  </div>
   <div class="swiper">
     <div class="swiper-wrapper">
       <div class="swiper-slide">
         <div class="slide-img">
-          <img src="https://picsum.photos/id/1041/1200/640" alt="img1">
+          <img src="<?php echo get_template_directory_uri() ?>/assets/img/common/mv-img1.jpg" alt="ビルに青空が映っている写真">
         </div>
       </div>
       <div class="swiper-slide">
         <div class="slide-img">
-          <img src="https://picsum.photos/id/1016/1200/640" alt="img2">
+          <img src="<?php echo get_template_directory_uri() ?>/assets/img/common/mv-img2.jpg" alt="４つのビルの中心で空を見上げている写真">
         </div>
       </div>
       <div class="swiper-slide">
         <div class="slide-img">
-          <img src="https://picsum.photos/id/1056/1200/640" alt="img3">
+          <img src="<?php echo get_template_directory_uri() ?>/assets/img/common/mv-img3.jpg" alt="高層ビルが並んでいる空撮写真">
         </div>
       </div>
     </div>
-    <!-- Add Pagination -->
-    <div class="swiper-pagination"></div>
   </div>
 </div>
 
-<!-- 最新のニュース1件 -->
-<div class="p-test-content">
-<p class="p-test">ニュース</p>
-  <div class="p-topics__items">
+<section class="p-news l-news">
+  <div class="p-news__inner">
     <?php
     $news_query = new WP_Query(
       array(
         'post_type'      => 'post',
         'posts_per_page' => 1,
-        )
-      );
+      )
+    );
     ?>
-    <?php if ( $news_query->have_posts() ) : ?>
-    <?php while ( $news_query->have_posts() ) : ?>
-    <?php $news_query->the_post(); ?>
-    
-    <div class="p-topics__item p-topic-info">
-      <time datetime="<?php the_time( 'c' );?>" class="p-topic-info__date"><?php the_time('Y.m.d'); ?></time>
-      <?php $category = get_the_category(); ?>
-      <span><?php echo $category[0]->cat_name; ?></span>
-      <a href="<?php the_permalink();?>" class="p-topic-info__text"> <?php the_title(); ?></a>
-      <a href="<?php echo $news; ?>">すべて見る</a>
-    </div>
-    
-    <?php endwhile; ?>
+    <?php if ($news_query->have_posts()) : ?>
+      <?php while ($news_query->have_posts()) : ?>
+        <?php $news_query->the_post(); ?>
+        <div class="p-news__info p-news-info">
+          <div class="p-news-info__box">
+            <time class="p-news-info__date" datetime="<?php the_time('c'); ?>"><?php the_time('Y.m.d'); ?></time>
+            <?php $category = get_the_category(); ?>
+            <span class="p-news-info__category"><?php echo $category[0]->cat_name; ?></span>
+          </div>
+          <a class="p-news-info__title" href="<?php the_permalink(); ?>"> <?php the_title(); ?></a>
+        </div>
+      <?php endwhile; ?>
     <?php endif; ?>
     <?php wp_reset_postdata(); ?>
+    <a class="p-news__button" href="<?php echo $news; ?>">すべて見る</a>
   </div>
-</div>
+</section>
+
 
 <!-- 事業内容 -->
 <div class="p-test-content">
@@ -85,16 +88,16 @@ $contact = esc_url( home_url( '/contact/' ) );
       array(
         'post_type'      => 'works',
         'posts_per_page' => 3,
-        )
-      );
+      )
+    );
     ?>
-    <?php if ( $works_query->have_posts() ) : ?>
-    <?php while ( $works_query->have_posts() ) : ?>
-    <?php $works_query->the_post(); ?>
+    <?php if ($works_query->have_posts()) : ?>
+      <?php while ($works_query->have_posts()) : ?>
+        <?php $works_query->the_post(); ?>
 
-      <?php the_post_thumbnail('thumbnail'); ?>
-    
-    <?php endwhile; ?>
+        <?php the_post_thumbnail('thumbnail'); ?>
+
+      <?php endwhile; ?>
     <?php endif; ?>
     <?php wp_reset_postdata(); ?>
 
@@ -117,28 +120,28 @@ $contact = esc_url( home_url( '/contact/' ) );
       array(
         'post_type'      => 'blog',
         'posts_per_page' => 3,
-        )
-      );
+      )
+    );
     ?>
-    <?php if ( $works_query->have_posts() ) : ?>
-    <?php while ( $works_query->have_posts() ) : ?>
-    <?php $works_query->the_post(); ?>
+    <?php if ($works_query->have_posts()) : ?>
+      <?php while ($works_query->have_posts()) : ?>
+        <?php $works_query->the_post(); ?>
 
-    <div>
-      <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('thumbnail'); ?></a>
-      <time datetime="<?php the_time('c'); ?>" class="p-topic-info__date"><?php the_time('Y.m.d'); ?></time>
-      <?php
-        $terms = get_the_terms($post->ID,'blog_category');
-        foreach( $terms as $term ) {
-          echo $term->name;
-        }
-        ?>
-        <a href="<?php the_permalink(); ?>" class="p-topic-info__text"> <?php the_title(); ?></a>
-        <!-- 記事の抜粋を表示、５５文字まで！！ -->
-        <?php the_excerpt(); ?>
-    </div>
-    
-    <?php endwhile; ?>
+        <div>
+          <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('thumbnail'); ?></a>
+          <time datetime="<?php the_time('c'); ?>" class="p-topic-info__date"><?php the_time('Y.m.d'); ?></time>
+          <?php
+          $terms = get_the_terms($post->ID, 'blog_category');
+          foreach ($terms as $term) {
+            echo $term->name;
+          }
+          ?>
+          <a href="<?php the_permalink(); ?>" class="p-topic-info__text"> <?php the_title(); ?></a>
+          <!-- 記事の抜粋を表示、５５文字まで！！ -->
+          <?php the_excerpt(); ?>
+        </div>
+
+      <?php endwhile; ?>
     <?php endif; ?>
     <?php wp_reset_postdata(); ?>
 
